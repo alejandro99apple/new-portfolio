33d003e55192bc99fb1dd5a5d913f4ae499bc659
alejandro99apple <alejandro99apple@gmail.com>
2025-01-02 03:03:20 -0600

    todo bien hasta aqui

--- src/app/components/contact-me/contact-me.component.ts
+++ src/app/components/contact-me/contact-me.component.ts
@@ -1,11 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import emailjs from 'emailjs-com';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-contact-me',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.css'
 })
 export class ContactMeComponent {
+  contactForm: FormGroup;
+  isBrowser: boolean;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+    this.isBrowser = typeof window !== 'undefined' && typeof document !== 'undefined';
+  }
+
+  ngOnInit(): void {
+    if (this.isBrowser) {
+      // Código que accede a document solo se ejecuta en el navegador
+      this.addClickEventToLinks();
+    }
+  }
+
+  addClickEventToLinks(): void {
+    // Ejemplo de código que accede a document
+    const links = document.querySelectorAll('a');
+    links.forEach(link => {
+      link.addEventListener('click', () => {
+        console.log('Link clicked');
+      });
+    });
+  }
+
+  public sendEmail(e: Event) {
+    e.preventDefault();
+
+    if (this.contactForm.valid) {
+      emailjs
+        .sendForm('service_79mp9bc', 'template_lb7yb0z', e.target as HTMLFormElement, 'aLGckq1KsJFYJ6E1m')
+        .then(
+          () => {
+            Swal.fire({
+              icon: 'success',
+              title: 'Message Sent',
+              text: 'Your message has been sent successfully.',
+              confirmButtonText: 'OK'
+            }).then(() => {
+              this.contactForm.reset(); // Reiniciar el formulario
+            });
+          },
+          () => {
+            Swal.fire({
+              icon: 'error',
+              title: 'Error',
+              text: 'There was an error sending your message. Please try again later.',
+              confirmButtonText: 'OK'
+            });
+          },
+        );
+    } else {
+      this.contactForm.markAllAsTouched();
+      Swal.fire({
+        icon: 'warning',
+        title: 'Incomplete Form',
+        text: 'Please fill out all fields correctly.',
+        confirmButtonText: 'OK'
+      });
+    }
+  }
 }
