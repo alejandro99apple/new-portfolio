108b5a9f9aaf0872efc3188241d93b688b489b31
alejandro99apple <alejandro99apple@gmail.com>
2025-01-01 22:15:30 -0600

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactMeComponent,
+    ProjectsComponent
+
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
