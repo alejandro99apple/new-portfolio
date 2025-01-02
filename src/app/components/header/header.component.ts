f6c2988b99dd6d88db67f033e2b009df0fce148c
alejandro99apple <alejandro99apple@gmail.com>
2025-01-01 22:30:08 -0600

    Primer commit

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  ngOnInit(): void {
+    this.addClickEventToLinks();
+  }
+
+  addClickEventToLinks(): void {
+    const links: NodeListOf<HTMLElement> = this.el.nativeElement.querySelectorAll('.navbar-nav .nav-link');
+    const navbarCollapse = this.el.nativeElement.querySelector('.navbar-collapse') as HTMLElement;
+
+    links.forEach((link: HTMLElement) => {
+      this.renderer.listen(link, 'click', () => {
+        if (navbarCollapse.classList.contains('show')) {
+          this.renderer.removeClass(navbarCollapse, 'show');
+        }
+      });
+    });
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event): void {
+    const navbarCollapse = this.el.nativeElement.querySelector('.navbar-collapse') as HTMLElement;
+    if (navbarCollapse.classList.contains('show')) {
+      this.renderer.removeClass(navbarCollapse, 'show');
+    }
+  }
 }
